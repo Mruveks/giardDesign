9f3626e5ad53e78d62b68d4a861cfaf6f0f7fdf8
Kuba Mrowiec <mruvekss@gmail.com>
2023-08-22 13:04:24 +0200

    Add masonry gallery

---
+++ dist/scripts/floatingButton.js
@@ -0,0 +1,21 @@
+const expandButton = document.getElementById("expandButton");
+const container = document.getElementById("grid-container");
+const gradient = document.getElementById("gradient");
+
+expandButton.addEventListener("click", () => {
+	if (container.classList.contains("height")) {
+		container.classList.remove("height");
+		gradient.style.opacity = "0";
+		container.classList.add("expanded");
+		container.classList.remove("closed");
+		expandButton.innerHTML = 'Close <img src="img/arrow-down.svg" class="rotate-180"/>';
+	} else {
+		container.classList.add("height");
+		gradient.style.opacity = "1";
+		container.classList.remove("expanded");
+		container.classList.add("closed");
+    expandButton.innerHTML = "Expand";
+    expandButton.innerHTML = 'Expand <img src="img/arrow-down.svg">';
+
+	}
+});
